30c425de5db6a4de87df8ac7cd2075f683bf3b8f
Gwendal <gwendou95@hotmail.fr>
2021-12-17 17:08:09 +0100

    Add some docs :pencil:

--- src/main/scala/progfun/core/Mower.scala
+++ src/main/scala/progfun/core/Mower.scala
@@ -6,6 +6,10 @@ case class Mower (
   land: Land
 ) {
 
+  /**
+   * Return the mower after executing all the actions
+   * @return
+   */
   def run: Mower = {
     this.actions
       .map(action => doAction(action))
@@ -13,7 +17,12 @@ case class Mower (
       .lastOption.getOrElse(this)
   }
 
-  def doAction(action: Char): Position = action match {
+  /**
+   * Return the mower position after executing the action
+   * @param action the action to execute
+   * @return the new position
+   */
+  private def doAction(action: Char): Position = action match {
       case 'A' => this.position.move(this.land)
       case 'G' | 'D' => this.position.turn(action)
     }
