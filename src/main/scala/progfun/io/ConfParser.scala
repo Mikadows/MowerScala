2bfcc950efb0c038aebe2c5ba5319ab9bb1079a5
Gwendal <gwendou95@hotmail.fr>
2021-12-17 01:27:26 +0100

    Code cleaning

--- src/main/scala/progfun/io/ConfParser.scala
+++ src/main/scala/progfun/io/ConfParser.scala
@@ -1,12 +1,13 @@
 package progfun.io
 
 import progfun.core.{Land, Mower, Position}
+import scala.annotation.tailrec
 
 object ConfParser {
 
   /**
    * Parse a configuration and return the Land.
-   * @param conf
+   * @param conf the configuration to parse as list of strings for each line.
    * @return Land
    */
   def getLand(conf: List[String]): Land = {
@@ -14,19 +15,32 @@ object ConfParser {
     Land(landLine(0).toInt, landLine(1).toInt)
   }
 
-  // Get mowers from the configuration
+  /**
+   * Parse the configuration and return the list of Mowers.
+   * @param conf the configuration to parse as list of strings for each line.
+   * @param land the Land to use by the Mowers.
+   * @return List[Mower]
+   */
   def getMowers(conf: List[String], land: Land): List[Mower] = {
     mowerConfParser(conf.drop(1), land, List())
   }
 
-  def mowerConfParser(conf: List[String], land: Land, acc: List[Mower]): List[Mower] = conf match {
+  /**
+   * Parse the mower with recursive call.
+ *
+   * @param conf the configuration to parse as list of strings for each line.
+   * @param land the Land to use by the Mowers.
+   * @param acc the list of Mowers to return.
+   * @return List[Mower]
+   */
+  @tailrec
+  private def mowerConfParser(conf: List[String], land: Land, acc: List[Mower]): List[Mower] = conf match {
     case Nil => acc
     case position :: actions =>
       val posRow = position.split(" ")
       val pos = Position(posRow(0).toInt, posRow(1).toInt, posRow(2).charAt(0))
       val actionList = actions(0).toList
-      println("Actions "+ actionList.toString())
-      val mower = new Mower(pos, actionList, land)
+      val mower = Mower(pos, actionList, land)
       mowerConfParser(actions.drop(1), land, mower :: acc)
   }
 
